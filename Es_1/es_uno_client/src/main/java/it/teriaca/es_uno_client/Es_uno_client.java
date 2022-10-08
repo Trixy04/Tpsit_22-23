d3531d7b7825e682b654acd4051be94919f6baa1
Trixy04 <teriaca.mattia@gmail.com>
2022-10-08 14:41:24 +0200

    Modifiche

--- Es_1/es_uno_client/src/main/java/it/teriaca/es_uno_client/Es_uno_client.java
+++ Es_1/es_uno_client/src/main/java/it/teriaca/es_uno_client/Es_uno_client.java
@@ -19,20 +19,22 @@ public class Es_uno_client {
     {
         Socket s= new Socket("localhost",3000);
         PrintWriter pr = new PrintWriter(s.getOutputStream());
-        /*pr.println("ci sono");
-        pr.flush();*/
 
-
-        BufferedReader altezza =new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Inserisci la tua altezza (M): ");
-        String stringAltezza= altezza.readLine();
+        BufferedReader inputStrem = new BufferedReader(new InputStreamReader(s.getInputStream())); 
+        String str = inputStrem.readLine();
         
-        BufferedReader peso = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Inserisci il tuo peso (KG): ");
-        String stringPeso = peso.readLine();
-
+        BufferedReader altezza =new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Server --> "+ str);
+        String stringAltezza = altezza.readLine();
         pr.println(stringAltezza);
         pr.flush();
+
+        BufferedReader inputStrem2 = new BufferedReader(new InputStreamReader(s.getInputStream())); 
+        String str2 = inputStrem2.readLine();
+
+        BufferedReader peso = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Server --> "+ str2);
+        String stringPeso = peso.readLine();
         pr.println(stringPeso);
         pr.flush();
 
@@ -40,9 +42,14 @@ public class Es_uno_client {
         BufferedReader br = new BufferedReader(in);*/
 
         BufferedReader inputStrem1 = new BufferedReader(new InputStreamReader(s.getInputStream())); 
-
         String str1 = inputStrem1.readLine();
-        System.out.println("Server: "+ str1);
+        System.out.println("Server --> "+ str1);
+
+        BufferedReader inputStrem3 = new BufferedReader(new InputStreamReader(s.getInputStream())); 
+        String str3 = inputStrem3.readLine();
+        System.out.println("Server --> " + str3);
+
+        s.close();
 
     }
 
