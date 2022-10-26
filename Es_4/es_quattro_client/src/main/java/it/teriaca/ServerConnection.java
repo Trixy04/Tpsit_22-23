a3831207a84ce7bf318b9eb5c1c39b1eead20a2a
lai2-01 <lai2-01@itismeucci.com>
2022-10-26 10:42:09 +0200

    Caricamento Es_4

---
+++ Es_4/es_quattro_client/src/main/java/it/teriaca/ServerConnection.java
@@ -0,0 +1,36 @@
+package it.teriaca;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ServerConnection implements Runnable {
+
+    private Socket s;
+    BufferedReader in;
+
+    public ServerConnection(Socket s) throws IOException {
+        this.s = s;
+        this.in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+    }
+
+    @Override
+    public void run() {
+        boolean running = true;
+
+        try {
+            while (running) {
+                String serverResponse = in.readLine();
+                if (serverResponse != null) {
+                    System.out.println(serverResponse);
+                    break;
+                }
+                break;
+            }
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+}
